9dccfc01cb0c47c0a5bb0285c32a1088509e1d70
RoninsWork <yurkaronin@gmail.com>
2023-10-24 17:56:07 +0300

    Заканчиваю стилизацию мобильного меню и адаптив шапки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,44 @@
 console.log('Файл подключён!');
 document.addEventListener("DOMContentLoaded", () => {
   console.log('Загрузка странички завершена!');
+
+  // показ мобильного меню и кнопки
+  const buttonMenu = document.querySelector('.button-menu');
+  // const navigation = document.querySelector('.navigation');
+  buttonMenu.addEventListener('click', function () {
+    buttonMenu.classList.toggle('isActive');
+    // navigation.classList.toggle('isActive');
+    document.body.classList.toggle('menu-open');
+  });
+
+
+// Гибридный аккордеон в мобильном меню
+// Находим все ссылки с классом .js-accordion-link
+let links = document.querySelectorAll('.js-accordion-link');
+
+// Добавляем слушатель события 'click' для каждой ссылки
+links.forEach(link => {
+    link.addEventListener('click', function(event) {
+        // Предотвращаем действие по умолчанию (если это обычная ссылка)
+        event.preventDefault();
+
+        // Получаем родительский элемент
+        let parent = this.parentElement;
+
+        // Проверяем, есть ли у родителя класс 'active'
+        if (parent.classList.contains('menu-accordion-active')) {
+            // Если класс есть - удаляем его
+            parent.classList.remove('menu-accordion-active');
+        } else {
+            // Если класса нет - добавляем его
+            parent.classList.add('menu-accordion-active');
+        }
+    });
+});
+
+
+
+
 });
 
 // техническая часть - УДАЛИТЬ НА ПРОДАКШЕНЕ!
